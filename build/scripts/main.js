99016cb0aa67d9210fa69d98514b76e9c39241a7
Benson Imoh,ST <benson@stbensonimoh.com>
2020-01-22 18:21:58 +0100

    Add International Telephone input

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -3,3 +3,12 @@
 particlesJS.load('particles-js', 'scripts/particles.json', function () {
   console.log('callback - particles.js config loaded')
 })
+// Add intl-tel-input
+window.intlTelInputGlobals.loadUtils('scripts/utils.js')
+var input = document.querySelector('#phone')
+window.intlTelInput(input, {
+  initialCountry: 'ng',
+  separateDialCode: true,
+  hiddenInput: 'full_phone',
+  utilsScript: 'scripts/utils.js'
+})
