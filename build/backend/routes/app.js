9f54512b2965bb99c4a6192183cef47bbe30f98d
Benson Imoh,ST <benson@stbensonimoh.com>
2020-01-23 19:46:27 +0100

    add new route

--- build/backend/routes/app.js
+++ build/backend/routes/app.js
@@ -1,23 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    console.log('server started successfully');
-    next();
+  console.log('server started successfully')
+  next()
 })
 app.get('/', (req, res) => {
-  res.send('Working on the server');
+  res.send('Working on the server')
 })
 
-module.exports = app;
+app.get('/okay', (req, res) => {
+  res.send('yeah!! server working')
+})
+
+module.exports = app
