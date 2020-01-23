5131c08b58492e6631206a1740d1a451ea70a5b9
Benson Imoh,ST <benson@stbensonimoh.com>
2020-01-23 20:02:26 +0100

    Add hostname to server

--- build/backend/app.js
+++ build/backend/app.js
@@ -1,7 +1,10 @@
-const PORT = process.env.PORT || 3000;
-const http = require('http');
+const PORT = process.env.PORT || 3000
+const hostname = '127.0.0.1'
+const http = require('http')
 const app = require('./routes/app')
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
-server.listen(PORT)
+server.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
