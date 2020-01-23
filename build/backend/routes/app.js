28ee51543d3aef7cd47f75e6160fe25e06ca9401
Osumgba Chiamaka <osumgbachiamaka@gmail.com>
2020-01-23 10:26:58 +0100

    Backend server created

---
+++ build/backend/routes/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    console.log('server started successfully');
+    next();
+})
+
+module.exports = app;
