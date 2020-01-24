af55985c5057432c019877aecb5e29ea29c511f4
Benson Imoh,ST <benson@stbensonimoh.com>
2020-01-24 16:43:36 +0100

    Fix configuration files

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,7 @@
-"use strict";
 // Load Particle.js
 // @ts-ignore
 particlesJS.load("particles-js", "scripts/particles.json", () => {
-    console.log("callback - particles.js config loaded");
+  console.log("callback - particles.js config loaded");
 });
 // Add intl-tel-input
 // @ts-ignore
@@ -10,8 +9,8 @@ window.intlTelInputGlobals.loadUtils("scripts/utils.js");
 var input = document.querySelector("#phone");
 //@ts-ignore
 window.intlTelInput(input, {
-    initialCountry: "ng",
-    separateDialCode: true,
-    hiddenInput: "full_phone",
-    utilsScript: "scripts/utils.js"
+  initialCountry: "ng",
+  separateDialCode: true,
+  hiddenInput: "full_phone",
+  utilsScript: "scripts/utils.js"
 });
