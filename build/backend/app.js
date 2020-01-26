f107adb8cbedb0a7f0cbeb04fd005c8fbf94309c
Benson Imoh,ST <benson@stbensonimoh.com>
2020-01-27 00:52:18 +0100

    Configure TypeORM and create first entity

--- build/backend/app.js
+++ build/backend/app.js
@@ -1,9 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const port = process.env.PORT || 3000;
 const hostname = '127.0.0.1';
-const http = require('http');
-const expressapp = require('./routes/app');
-const server = http.createServer(expressapp);
+const http_1 = require("http");
+const app_1 = require("./routes/app");
+const server = http_1.createServer(app_1.app);
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
--- build/backend/routes/app.js
+++ build/backend/routes/app.js
@@ -1,17 +1,22 @@
 "use strict";
-const config = require('./config');
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const indexRouter = express.Router();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_1 = __importDefault(require("./config"));
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = express_1.default();
+exports.app = app;
+const indexRouter = express_1.default.Router();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
 app.use((req, res, next) => {
     console.log("server started successfully");
     next();
@@ -28,5 +33,4 @@ indexRouter.get("/chiamaka", (req, res) => {
 indexRouter.get("/work", (req, res) => {
     res.send("Work is great!");
 });
-app.use(config.baseUrl, indexRouter);
-module.exports = app;
+app.use(config_1.default.baseUrl, indexRouter);
